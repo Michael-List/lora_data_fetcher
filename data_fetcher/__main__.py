4baa82336a2a512be7616f7cb8469fe2a060982b
Michael List <michael.list@stud.fh-rosenheim.de>
2019-04-29 21:15:24 +0200

    First test sending data to influxdb

--- data_fetcher/__main__.py
+++ data_fetcher/__main__.py
@@ -15,12 +15,15 @@ if __name__ == '__main__':
     logger = logging.getLogger(__name__)
 
     with open('/dev/shm/receive_fifo', "rb") as f:
+        api_client = ApiClient()
+
         while True:
             # Read is blocking
             chunk = f.read(52)
             if chunk:
                 logger.debug('Received message from lora: ' + chunk.decode("utf-8"))
                 ws_data = WeatherstationData.validate_string(chunk.decode("utf-8"))
+                api_client.send_data(db='weatherstation', ws_data=ws_data)
                 #             if ws_data is not None:
                 #                 print(ws_data.timestamp)
                 #                 print(ws_data.station)
