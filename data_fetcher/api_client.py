5e00e79108d03734a98af6c392dedc8034a8cfeb
Michael List <michael.list@stud.fh-rosenheim.de>
2019-04-29 21:18:06 +0200

    Cast int to string for post request

--- data_fetcher/api_client.py
+++ data_fetcher/api_client.py
@@ -61,19 +61,19 @@ class ApiClient:
             raise Exception('db cannot be None')
 
         if ws_data.temperature:
-            r = self.session.post(url=self.api_url, params={'db': db}, data='temperature,station=' + ws_data.station + ' value=' + str(ws_data.temperature) + ' ' + str(int((ws_data.timestamp.timestamp() * 1e+9))))
+            r = self.session.post(url=self.api_url, params={'db': db}, data='temperature,station=' + str(ws_data.station) + ' value=' + str(ws_data.temperature) + ' ' + str(int((ws_data.timestamp.timestamp() * 1e+9))))
             if r.status_code not in range(200, 300):
                 logging.warning('Could not post temperature, status code was: {}'.format(r.status_code))
                 successful = False
 
         if ws_data.humidity:
-            r = self.session.post(url=self.api_url, params={'db': db}, data='humidity,station=' + ws_data.station + ' value=' + str(ws_data.humidity) + ' ' + str(int((ws_data.timestamp.timestamp() * 1e+9))))
+            r = self.session.post(url=self.api_url, params={'db': db}, data='humidity,station=' + str(ws_data.station) + ' value=' + str(ws_data.humidity) + ' ' + str(int((ws_data.timestamp.timestamp() * 1e+9))))
             if r.status_code not in range(200, 300):
                 logging.warning('Could not post humidity, status code was: {}'.format(r.status_code))
                 successful = False
 
         if ws_data.pressure:
-            r = self.session.post(url=self.api_url, params={'db': db}, data='pressure,station=' + ws_data.station + ' value=' + str(ws_data.pressure) + ' ' + str(int((ws_data.timestamp.timestamp() * 1e+9))))
+            r = self.session.post(url=self.api_url, params={'db': db}, data='pressure,station=' + str(ws_data.station) + ' value=' + str(ws_data.pressure) + ' ' + str(int((ws_data.timestamp.timestamp() * 1e+9))))
             if r.status_code not in range(200, 300):
                 logging.warning('Could not post pressure, status code was: {}'.format(r.status_code))
                 successful = False
