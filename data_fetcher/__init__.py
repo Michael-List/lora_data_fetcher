0427e332b4c31f07a679339b4b75d2d62c3b92bd
Michael List <michael.list@stud.fh-rosenheim.de>
2019-04-29 21:05:56 +0200

    Remove unused pipeline_path

--- data_fetcher/__init__.py
+++ data_fetcher/__init__.py
@@ -7,5 +7,3 @@ if not os.getenv('RUNNING_INSIDE_DOCKER', False):
     load_dotenv(verbose=True)
     env_path = Path('.') / '.env'
     load_dotenv(dotenv_path=env_path)
-
-pipeline_path = os.environ['PIPELINE_PATH']
