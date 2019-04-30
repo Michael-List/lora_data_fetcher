a0d8cbe482019e47cfee5f2f95ae8a3826accf3e
Michael List <michael.list@stud.fh-rosenheim.de>
2019-04-30 07:04:25 +0200

    Code cleanup fo api_client

--- data_fetcher/__main__.py
+++ data_fetcher/__main__.py
@@ -1,13 +1,6 @@
 import logging
-from time import sleep
-import sys
-import time
-from datetime import datetime
-
-from watchdog.observers import Observer
 
 from data_fetcher.api_client import ApiClient
-from data_fetcher.send_sensor_data import FSHandler
 from data_fetcher.weatherstation_data import WeatherstationData
 
 if __name__ == '__main__':
@@ -23,27 +16,6 @@ if __name__ == '__main__':
             if chunk:
                 logger.debug('Received message from lora: ' + chunk.decode("utf-8"))
                 ws_data = WeatherstationData.validate_string(chunk.decode("utf-8"))
-                api_client.send_data(db='weatherstation', ws_data=ws_data)
-                #             if ws_data is not None:
-                #                 print(ws_data.timestamp)
-                #                 print(ws_data.station)
-                #                 print(ws_data.temperature)
 
-    # ws_data = WeatherstationData(datetime.now(), 1, 20.33)
-    # print(int(datetime.now().timestamp() * 1e+9))
-    #
-    # api_client = ApiClient()
-    # api_client.send_data(db='weatherstation', ws_data=ws_data)
-
-    # path = sys.argv[1] if len(sys.argv) > 1 else '.'
-    # api_client = ApiClient()
-    # event_handler = FSHandler(api_client)
-    # observer = Observer()
-    # observer.schedule(event_handler, path, recursive=True)
-    # observer.start()
-    # try:
-    #     while True:
-    #         time.sleep(1)
-    # except KeyboardInterrupt:
-    #     observer.stop()
-    # observer.join()
+                if ws_data is not None:
+                    api_client.send_data(db='weatherstation', ws_data=ws_data)
--- data_fetcher/api_client.py
+++ data_fetcher/api_client.py
@@ -1,55 +1,13 @@
-import sys
-import json
 import logging
 
 import requests
 
-SERVER_URL = 'http://influxdb:8086/write'
-SERVER_USER = 'user'
-SERVER_PASSWORD = 'user'
-
 
 class ApiClient:
-    token = None
-    api_url = SERVER_URL
-    session = requests.Session()
+    api_url = 'http://influxdb:8086/write'
 
     def __init__(self):
-        # if self.token:
-        #     self.session.headers.update({'Authorization': self.token})
-        # else:
-        #     # if no token, do login
-        #     if not self.token:
-        #         try:
-        #             self._login()
-        #         except requests.HTTPError:
-        #             sys.exit('Username-password invalid')
-        pass
-
-    def _login(self):
-        headers = {'content-type': 'application/json'}
-        payload = {
-            'username': SERVER_USER,
-            'password': SERVER_PASSWORD
-        }
-        r = requests.post(self.api_url + '/token/generate-token',
-                          data=json.dumps(payload),
-                          headers=headers)
-
-        # raise exception if cannot login
-        r.raise_for_status()
-
-        # save token and update session
-        self.token = r.json()['token']
-        self.session.headers.update({'Authorization': 'Bearer ' + self.token})
-
-    def _test_auth(self):
-        r = self.session.get(self.api_url + '/api/temperature/1')
-        # TODO: Make it more beautiful and error proof
-        if r.status_code != 401:
-            return True
-        else:
-            return False
+        self.logger = logging.getLogger(__name__)
 
     def send_data(self, db, ws_data):
         successful = True
@@ -60,42 +18,33 @@ class ApiClient:
         if db is None:
             raise Exception('db cannot be None')
 
+        timestamp = int((ws_data.timestamp.timestamp() * 1e+9))
+
         if ws_data.temperature:
-            r = self.session.post(url=self.api_url, params={'db': db}, data='temperature,station=' + str(ws_data.station) + ' value=' + str(ws_data.temperature) + ' ' + str(int((ws_data.timestamp.timestamp() * 1e+9))))
+            r = requests.post(url=self.api_url,
+                              params={'db': db},
+                              data='temperature,station=' + str(ws_data.station) +
+                                   ' value=' + str(ws_data.temperature) + ' ' + str(timestamp))
             if r.status_code not in range(200, 300):
                 logging.warning('Could not post temperature, status code was: {}'.format(r.status_code))
                 successful = False
 
         if ws_data.humidity:
-            r = self.session.post(url=self.api_url, params={'db': db}, data='humidity,station=' + str(ws_data.station) + ' value=' + str(ws_data.humidity) + ' ' + str(int((ws_data.timestamp.timestamp() * 1e+9))))
+            r = requests.post(url=self.api_url,
+                              params={'db': db},
+                              data='humidity,station=' + str(ws_data.station) +
+                                   ' value=' + str(ws_data.humidity) + ' ' + str(timestamp))
             if r.status_code not in range(200, 300):
                 logging.warning('Could not post humidity, status code was: {}'.format(r.status_code))
                 successful = False
 
         if ws_data.pressure:
-            r = self.session.post(url=self.api_url, params={'db': db}, data='pressure,station=' + str(ws_data.station) + ' value=' + str(ws_data.pressure) + ' ' + str(int((ws_data.timestamp.timestamp() * 1e+9))))
+            r = requests.post(url=self.api_url,
+                              params={'db': db},
+                              data='pressure,station=' + str(ws_data.station) +
+                                   ' value=' + str(ws_data.pressure) + ' ' + str(timestamp))
             if r.status_code not in range(200, 300):
                 logging.warning('Could not post pressure, status code was: {}'.format(r.status_code))
                 successful = False
-
-        # if humidity:
-        #     payload = {
-        #         "humidity": humidity,
-        #         "timestamp": timestamp
-        #     }
-        #     self.session.post(url=self.api_url + '/api/humidity', json=payload)
-        #     if r.status_code not in range(200, 300):
-        #         logging.warning('Could not post humidity, status code was: {}'.format(r.status_code))
-        #         successful = False
-        #
-        # if pressure:
-        #     payload = {
-        #         "pressure": pressure,
-        #         "timestamp": timestamp
-        #     }
-        #     self.session.post(url=self.api_url + '/api/pressure', json=payload)
-        #     if r.status_code not in range(200, 300):
-        #         logging.warning('Could not post pressure, status code was: {}'.format(r.status_code))
-        #         successful = False
 
         return successful
