f236b983bf0a74aeef0668144f918de459a7c424
Michael List <michael.list@stud.fh-rosenheim.de>
2019-07-27 14:24:36 +0200

    Fix NoneType bug in main

--- data_fetcher/__main__.py
+++ data_fetcher/__main__.py
@@ -43,26 +43,28 @@ if __name__ == '__main__':
                         api_client.send_ws_data(db='weatherstation', ws_data=ws_data)
                         last_ws_data = ws_data
 
-                if wm_match and not ws_match:
+                elif wm_match and not ws_match:
                     logger.debug('Found wm: ' + ws_match.group(0))
                     inc_data = inc_data[wm_match.span(0)[1]:]
                     wm_data = WashingmachineData.convert_string(ws_match.group(0))
                     api_client.send_wm_data(db='washingmachine', wm_data=wm_data)
 
-                if wm_match.span(0)[0] > ws_match.span(0)[0]:
-                    logger.debug('Found ws: ' + ws_match.group(0))
-                    inc_data = inc_data[ws_match.span(0)[1]:]
-                    ws_data = WeatherstationData.convert_string(ws_match.group(0))
+                elif ws_match and wm_match:
+                    if wm_match.span(0)[0] > ws_match.span(0)[0]:
+                        logger.debug('Found ws: ' + ws_match.group(0))
+                        inc_data = inc_data[ws_match.span(0)[1]:]
+                        ws_data = WeatherstationData.convert_string(ws_match.group(0))
 
-                    if ws_data is not None and (last_ws_data is None or ws_data.__ne__(last_ws_data)):
-                        logger.debug("weather data doesn't equal last weather data")
-                        api_client.send_ws_data(db='weatherstation', ws_data=ws_data)
-                        last_ws_data = ws_data
-                else:
-                    logger.debug('Found wm: ' + ws_match.group(0))
-                    inc_data = inc_data[wm_match.span(0)[1]:]
-                    wm_data = WashingmachineData.convert_string(ws_match.group(0))
-                    api_client.send_wm_data(db='washingmachine', wm_data=wm_data)
+                        if ws_data is not None and (last_ws_data is None or ws_data.__ne__(last_ws_data)):
+                            logger.debug("weather data doesn't equal last weather data")
+                            api_client.send_ws_data(db='weatherstation', ws_data=ws_data)
+                            last_ws_data = ws_data
+
+                    else:
+                        logger.debug('Found wm: ' + ws_match.group(0))
+                        inc_data = inc_data[wm_match.span(0)[1]:]
+                        wm_data = WashingmachineData.convert_string(ws_match.group(0))
+                        api_client.send_wm_data(db='washingmachine', wm_data=wm_data)
 
                 logger.debug('String after manipulating: ' + inc_data)
 
